a3f68ae64693a74172f9959ec10e113e7eab4df9
Robert Nagy <robert@mtier.org>
2021-08-28 17:03:07 +0200

    fix icon alignment in the navigation menu after the recent nextcloud update
    fixes #141

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,14 +1,13 @@
-<ul>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.page.index'));?>" class='nav-icon-timer'>Timer</a></li>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.dashboard.index'));?>" class='nav-icon-dashboard'>Dashboard</a></li>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.goals.index'));?>" class='nav-icon-goals'>Goals</a></li>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.reports.index'));?>"" class='nav-icon-reports'>Reports</a></li>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.timelines.index'));?>"" class='nav-icon-reports'>Timelines</a></li>
+<ul class="with-icon">
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.page.index'));?>" class='nav-icon-timer svg'>Timer</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.dashboard.index'));?>" class='nav-icon-dashboard svg'>Dashboard</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.goals.index'));?>" class='nav-icon-goals svg'>Goals</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.reports.index'));?>"" class='nav-icon-reports svg'>Reports</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.timelines.index'));?>"" class='nav-icon-reports svg'>Timelines</a></li>
 	<?php if (\OC_User::isAdminUser(\OC_User::getUser())): ?>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.timelinesAdmin.index'));?>"" class='nav-icon-reports'>Timelines Admin</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.timelinesAdmin.index'));?>"" class='nav-icon-reports svg'>Timelines Admin</a></li>
 	<?php endif; ?>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.projects.index'));?>" class='nav-icon-projects'>Projects</a></li>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.clients.index'));?>" class='nav-icon-clients'>Clients</a></li>
-	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.tags.index'));?>" class='nav-icon-tags'>Tags</a></li>
-
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.projects.index'));?>" class='nav-icon-projects svg'>Projects</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.clients.index'));?>" class='nav-icon-clients svg'>Clients</a></li>
+	<li><a href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('timetracker.tags.index'));?>" class='nav-icon-tags svg'>Tags</a></li>
 </ul>
