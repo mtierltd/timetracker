ba80e6681fd0841c5455cbef9027c7ccf6a2a950
MTier Ltd <info@mtier.org>
2022-10-26 09:35:05 +0200

    add support for NC 25 and bump version
    fixes #192

--- templates/index.php
+++ templates/index.php
@@ -1,36 +1,17 @@
 <?php
-//script('timetracker', "jquery-3.5.1.min");
-//script('timetracker', "jquery-3.5.0");
-/*
-script('timetracker', "kingtable");
-script('timetracker', "moment.min");
-script('timetracker', "daterangepicker.min");
-script('timetracker', "tabulator");
-script('timetracker', "Chart.min");
-script('timetracker', "select2.min");
-style('timetracker', "kingtable");
-style('timetracker', "daterangepicker");
-style('timetracker', "tabulator");
-*/
-//style('timetracker', "select2");
-
-//style('files', 'files');
 style('timetracker', 'all');
 style('timetracker', 'style');
 script('timetracker', $script);
 
 ?>
 
-<div id="app">
-	<div id="app-navigation">
-		<?php print_unescaped($this->inc('navigation/index')); ?>
-		<?php print_unescaped($this->inc('settings/index')); ?>
-	</div>
-
-	<div id="app-content">
-		<div id="app-content-wrapper">
-			<?php print_unescaped($this->inc($appPage)); ?>
-		</div>
-	</div>
+<div id="app-navigation">
+	<?php print_unescaped($this->inc('navigation/index')); ?>
+	<?php print_unescaped($this->inc('settings/index')); ?>
 </div>
 
+<div id="app-content">
+	<div id="app-content-wrapper">
+		<?php print_unescaped($this->inc($appPage)); ?>
+	</div>
+</div>
