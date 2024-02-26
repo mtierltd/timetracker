a3a740b35df079144ae9db614794a1709822e652
MTier Ltd <info@mtier.org>
2024-02-26 12:50:38 +0100

    add quick hack to make NC28 usable

--- templates/index.php
+++ templates/index.php
@@ -1,13 +1,20 @@
 <?php
+
+$nextcloudMajorVersion = \OCP\Util::getVersion()[0];
+
 style('timetracker', 'all');
 style('timetracker', 'style');
+
+if (intval($nextcloudMajorVersion) >= 28) {
+	style('timetracker', 'style-compat');
+}
+
 script('timetracker', $script);
 
 ?>
 
 <div id="app-navigation">
 	<?php print_unescaped($this->inc('navigation/index')); ?>
-	<?php print_unescaped($this->inc('settings/index')); ?>
 </div>
 
 <div id="app-content">
